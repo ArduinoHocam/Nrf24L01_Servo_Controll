f61cca00bf3454d1d172ce435f09249268b11cfe
ArduinoHocam <ArduinoHocam@users.noreply.github.com>
2017-02-23 17:48:20 +0200

    Codes

---
+++ nrf24l01_reciever(alici)/nrf24l01_reciever(alici).ino
@@ -0,0 +1,55 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include<Servo.h>
+
+Servo servo; //Servo object
+
+
+#define CE_PIN   9 //nrf24l01 PINS
+#define CSN_PIN 10
+#define led 4
+
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+RF24 radio(CE_PIN, CSN_PIN);
+
+int joystick[3]; //joystick variable with 3 element array
+
+void setup() {
+  pinMode(4, OUTPUT);
+  radio.begin();  //nrf24l01 started
+  radio.openReadingPipe(1, pipe);
+  radio.startListening();;
+  servo.attach(6);  //servo started
+  Serial.begin(9600);
+}
+
+void loop() {
+
+
+  if ( radio.available() ) //If signal available
+  {
+    radio.read( joystick, sizeof(joystick) );  //read joystick values
+
+    int  xpos = joystick[1];  
+    int button = joystick[2];
+    int xpos1 = map(xpos, 0, 1023, 0, 180);
+    bool done = false;  
+    while (!done) {  //listen till done become true
+      done = radio.read(joystick, 1);
+      servo.write (xpos1);
+      delay(10);
+    }
+    if (button == 0) { //led
+      digitalWrite(4, HIGH);
+
+    }
+    else {
+      digitalWrite(4, LOW);
+
+    }
+
+
+  }
+}
+
---
+++ nrf24l01_transmitter(verici)/nrf24l01_transmitter(verici).ino
@@ -0,0 +1,34 @@
+#include <SPI.h> 
+#include <nRF24L01.h>
+#include <RF24.h>
+
+//Pins....
+#define CE_PIN   9
+#define CSN_PIN 10
+#define JOYSTICKX A0 //X axsis connected to A0
+#define JOYSTICKY A1 
+#define BUTTON 8 
+
+
+const uint64_t pipe = 0xE8E8F0F0E1LL;  //channel identity
+RF24 radio(CE_PIN, CSN_PIN); 
+int joystick[3];  //3 element array
+
+void setup() 
+{
+  //Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(pipe);
+  pinMode(BUTTON, INPUT);
+}
+
+void loop()   
+{
+
+  joystick[0] = analogRead(JOYSTICKX);
+  joystick[1] = analogRead(JOYSTICKY);
+  joystick[2] = digitalRead(BUTTON);
+  radio.write( joystick, sizeof(joystick) ); //sending...
+ 
+
+}
